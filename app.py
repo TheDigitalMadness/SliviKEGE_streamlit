a53d8c690b6f5c9f26082987623af07a693c304b
TheDigitalMadness <114095740+TheDigitalMadness@users.noreply.github.com>
2025-03-11 21:54:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ variant_id = st.number_input('Номер варианта', min_value=0, value=1
 if st.button('Получить'):
     answers = get_ans_on_kim(int(variant_id))
     if isinstance(answers, list):
+        st.write(answers)
         for ans in answers:
             st.success(f'{ans["id"]}:')
             st.write(f'{ans["answer"]}')
